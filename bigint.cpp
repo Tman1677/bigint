d8ba40940a8f8da48d0f118efec9c5514f9c6253
Tman1677 <5393759+Tman1677@users.noreply.github.com>
2018-11-07 16:48:20 -0500

    minor commenting

--- bigint.cpp
+++ bigint.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <bitset>
+#include <iostream>
 
 std::bitset<8> add(std::bitset<8> a, std::bitset<8> b);
 
@@ -8,14 +8,15 @@ int main() {
 	std::bitset<8> test2;
 	test1 = 1;
 	test2 = 3;
-	
-	std::cout << add(test1,test2) << std::endl;
+
+	std::cout << add(test1, test2) << std::endl;
 }
 
-
 std::bitset<8> add(std::bitset<8> a, std::bitset<8> b) {
+	// First priority once this thing is running is optimizing this function as
+	// there has got to be a better way to do this
 	static std::bitset<8> temp;
-	while(!b.none()) {
+	while (!b.none()) {
 		temp = a ^ b;
 		b = (a & b) << 1;
 		a = std::move(temp);
