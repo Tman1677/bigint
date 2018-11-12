f1a3304e8b33292faa61892e91aedc3ca4f68109
Tman1677 <5393759+Tman1677@users.noreply.github.com>
2018-11-12 17:37:45 -0500

    basic negatives functionality added

--- bigint.cpp
+++ bigint.cpp
@@ -6,14 +6,28 @@ class bigint {
 	std::bitset<8> bits;
   public:
 	bigint(int);
+	bigint(const bigint&);
+	bigint(bigint&&);
+	bigint(const std::bitset<8>&);
+	bigint(std::bitset<8>&&);
+	bool operator[] (std::size_t index) const;
 
 	friend std::ostream& operator<<(std::ostream&, const bigint&);
 	friend bigint operator+(bigint object1, bigint object2);
+	friend bigint operator+(int primitive1, bigint object2);
+	friend bigint operator+(bigint object1, int primitive1);
+	friend bigint operator-(bigint object1);
 };
 
 std::ostream& operator<<(std::ostream& outstream, const bigint& object1) {
 	//this will have to change eventually to allow for bigger integers to be printed correctly
-	outstream << object1.bits.to_ullong();
+	//also it's pretty atrocious
+	if(object1.bits[8-1]==0) {
+		outstream << object1.bits.to_ullong();
+	} else {
+		outstream << -static_cast<long long>((-object1).bits.to_ullong());
+	}
+	
 	return outstream;
 }
 bigint::bigint(int primitive1): bits(primitive1) {}
@@ -29,4 +43,34 @@ bigint operator+(bigint object1, bigint object2) {
 	}
 
 	return std::move(object1);
+}
+bigint::bigint(const bigint& object1) {
+	bits = object1.bits;
+}
+bigint::bigint(bigint&& object1) {
+	bits = std::move(object1.bits);
+}
+bigint::bigint(const std::bitset<8>& primitive1) {
+	bits = primitive1;
+}
+bigint::bigint(std::bitset<8>&& primitve1) {
+	bits = std::move(primitve1);
+}
+bigint operator+(int primitive1, bigint object1) {
+	// needs further optimization
+	// creating a whole instance of this class is probably unnecessary overhead
+	return bigint(primitive1) + object1;
+}
+bigint operator+(bigint object1, int primitive1) {
+	// needs further optimization
+	// creating a whole instance of this class is probably unnecessary overhead
+	return bigint(primitive1) + object1;
+}
+bool bigint::operator[] (std::size_t index) const {
+	return bits[index];
+}
+bigint operator-(bigint object1) {
+	//needs optimization
+	object1.bits = ~object1.bits;
+	return object1 + 1;
 }
