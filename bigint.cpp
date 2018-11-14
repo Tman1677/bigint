afa5813b85cb47b9d574e71c4a55c38428f7bd9a
Tman1677 <5393759+Tman1677@users.noreply.github.com>
2018-11-13 22:00:29 -0500

    Definitely not working yet but save just in case

--- bigint.cpp
+++ bigint.cpp
@@ -1,57 +1,117 @@
 #include <bitset>
 #include <iostream>
 
+//forward declarations somehow fix the templating errors with friend functions
+// template <int N>
+// class bigint;
+
+// template <int N>
+// std::ostream& operator<<(std::ostream&, const bigint<N>&);
+// template <int N>
+// bigint<N> operator+(bigint<N>, bigint<N>);
+// template <int N>
+// bigint<N> operator+(int, bigint<N>);
+// template <int N>
+// bigint<N> operator+(bigint<N>, int);
+// template <int N>
+// bigint<N> operator-(bigint<N>);
+// template <int N>
+// bigint<N> operator-(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bigint<N> operator-(const bigint<N>&, int);
+// template <int N>
+// bigint<N> operator-(int, const bigint<N>&);
+// template <int N>
+// bigint<N> operator*(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bigint<N> operator*(const bigint<N>&, int);
+// template <int N>
+// bigint<N> operator*(int, const bigint<N>&);
+// template <int N>
+// bool operator<(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bool operator<(const bigint<N>&, int);
+// template <int N>
+// bool operator<(int, const bigint<N>&);
+// template <int N>
+// bool operator>(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bool operator>(const bigint<N>&, int);
+// template <int N>
+// bool operator>(int, const bigint<N>&);
+// template <int N>
+// bool operator<=(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bool operator<=(const bigint<N>&, int);
+// template <int N>
+// bool operator<=(int, const bigint<N>&);
+// template <int N>
+// bool operator>=(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bool operator>=(const bigint<N>&, int);
+// template <int N>
+// bool operator>=(int, const bigint<N>&);
+// template <int N>
+// bool operator==(const bigint<N>&, const bigint<N>&);
+// template <int N>
+// bool operator==(const bigint<N>&, int);
+// template <int N>
+// bool operator==(int, const bigint<N>&);
+
+template <int N>
 class bigint {
   private:
-	std::bitset<8> bits;
+	std::bitset<N> bits;
   public:
   	bigint();
 	bigint(int);
-	bigint(const bigint&);
-	bigint(bigint&&);
-	bigint(const std::bitset<8>&);
-	bigint(std::bitset<8>&&);
+	bigint(const bigint<N>&);
+	bigint(bigint<N>&&);
+	bigint(const std::bitset<N>&);
+	bigint(std::bitset<N>&&);
 	bool operator[] (std::size_t index) const;
 
-	friend bigint operator"" _bi(unsigned long long primitive1);
-	friend std::ostream& operator<<(std::ostream&, const bigint&);
-	friend bigint operator+(bigint, bigint);
-	friend bigint operator+(int, bigint);
-	friend bigint operator+(bigint, int);
-	bigint& operator+=(bigint);
-	bigint& operator+=(int);
-	friend bigint operator-(bigint);
-	friend bigint operator-(const bigint&, const bigint&);
-	friend bigint operator-(const bigint&, int);
-	friend bigint operator-(int, const bigint&);
-	bigint& operator-=(const bigint &);
-	bigint& operator-=(int);
-	friend bigint operator*(const bigint&, const bigint&);
-	friend bigint operator*(const bigint&, int);
-	friend bigint operator*(int, const bigint&);
-	bigint& operator*=(const bigint&);
-	bigint& operator*=(int);
-	friend bool operator<(const bigint&, const bigint&);
-	friend bool operator<(const bigint&, int);
-	friend bool operator<(int, const bigint&);
-	friend bool operator>(const bigint&, const bigint&);
-	friend bool operator>(const bigint&, int);
-	friend bool operator>(int, const bigint&);
-	friend bool operator<=(const bigint&, const bigint&);
-	friend bool operator<=(const bigint&, int);
-	friend bool operator<=(int, const bigint&);
-	friend bool operator>=(const bigint&, const bigint&);
-	friend bool operator>=(const bigint&, int);
-	friend bool operator>=(int, const bigint&);
-	friend bool operator==(const bigint&, const bigint&);
-	friend bool operator==(const bigint&, int);
-	friend bool operator==(int, const bigint&);
+	//friend bigint operator"" _bi(unsigned long long primitive1);
+	template <int M>
+	friend std::ostream& operator<<(std::ostream&, const bigint<N>&);
+	friend bigint<N> operator+(bigint<N>, bigint<N>);
+	friend bigint<N> operator+(int, bigint<N>);
+	friend bigint<N> operator+(bigint<N>, int);
+	bigint<N>& operator+=(bigint<N>);
+	bigint<N>& operator+=(int);
+	friend bigint<N> operator-(bigint<N>);
+	friend bigint<N> operator-(const bigint<N>&, const bigint<N>&);
+	friend bigint<N> operator-(const bigint<N>&, int);
+	friend bigint<N> operator-(int, const bigint<N>&);
+	bigint<N>& operator-=(const bigint<N>&);
+	bigint<N>& operator-=(int);
+	friend bigint<N> operator*(const bigint<N>&, const bigint<N>&);
+	friend bigint<N> operator*(const bigint<N>&, int);
+	friend bigint<N> operator*(int, const bigint<N>&);
+	bigint<N>& operator*=(const bigint<N>&);
+	bigint<N>& operator*=(int);
+	friend bool operator<(const bigint<N>&, const bigint<N>&);
+	friend bool operator<(const bigint<N>&, int);
+	friend bool operator<(int, const bigint<N>&);
+	friend bool operator>(const bigint<N>&, const bigint<N>&);
+	friend bool operator>(const bigint<N>&, int);
+	friend bool operator>(int, const bigint<N>&);
+	friend bool operator<=(const bigint<N>&, const bigint<N>&);
+	friend bool operator<=(const bigint<N>&, int);
+	friend bool operator<=(int, const bigint<N>&);
+	friend bool operator>=(const bigint<N>&, const bigint<N>&);
+	friend bool operator>=(const bigint<N>&, int);
+	friend bool operator>=(int, const bigint<N>&);
+	friend bool operator==(const bigint<N>&, const bigint<N>&);
+	friend bool operator==(const bigint<N>&, int);
+	friend bool operator==(int, const bigint<N>&);
 };
 
-std::ostream& operator<<(std::ostream& outstream, const bigint& object1) {
+template <int N>
+std::ostream& operator<<(std::ostream& outstream, const bigint<N>& object1) {
 	//this will have to change eventually to allow for bigger integers to be printed correctly
 	//also it's pretty atrocious
-	if(object1.bits[8-1]==0) {
+	if(object1.bits[N-1]==0) {
 		outstream << object1.bits.to_ullong();
 	} else {
 		outstream << -static_cast<long long>((-object1).bits.to_ullong());
@@ -59,28 +119,35 @@ std::ostream& operator<<(std::ostream& outstream, const bigint& object1) {
 	
 	return outstream;
 }
-bigint::bigint() {
+template <int N>
+bigint<N>::bigint() {
 	bits = 0;
 }
-bigint::bigint(int primitive1): bits(primitive1) {}
+template <int N>
+bigint<N>::bigint(int primitive1): bits(primitive1) {}
 
 
-bigint::bigint(const bigint& object1) {
+template <int N>
+bigint<N>::bigint(const bigint<N>& object1) {
 	bits = object1.bits;
 }
-bigint::bigint(bigint&& object1) {
+template <int N>
+bigint<N>::bigint(bigint<N>&& object1) {
 	bits = std::move(object1.bits);
 }
-bigint::bigint(const std::bitset<8>& primitive1) {
+template <int N>
+bigint<N>::bigint(const std::bitset<N>& primitive1) {
 	bits = primitive1;
 }
-bigint::bigint(std::bitset<8>&& primitve1) {
+template <int N>
+bigint<N>::bigint(std::bitset<N>&& primitve1) {
 	bits = std::move(primitve1);
 }
-bigint operator+(bigint object1, bigint object2) {
+template <int N>
+bigint<N> operator+(bigint<N> object1, bigint<N> object2) {
 	// First priority once this thing is running is optimizing this function as
 	// there has got to be a better way to do this
-	std::bitset<8> temp;
+	std::bitset<N> temp;
 	while (!object2.bits.none()) {
 		temp = object1.bits ^ object2.bits;
 		object2.bits = (object1.bits & object2.bits) << 1;
@@ -89,124 +156,147 @@ bigint operator+(bigint object1, bigint object2) {
 
 	return std::move(object1);
 }
-bigint operator+(int primitive1, bigint object1) {
+template <int N>
+bigint<N> operator+(int primitive1, bigint<N> object1) {
 	// needs further optimization
 	// creating a whole instance of this class is probably unnecessary overhead
 	return bigint(primitive1) + object1;
 }
-bigint operator+(bigint object1, int primitive1) {
+template <int N>
+bigint<N> operator+(bigint<N> object1, int primitive1) {
 	// needs further optimization
 	// creating a whole instance of this class is probably unnecessary overhead
 	return bigint(primitive1) + object1;
 }
-bool bigint::operator[] (std::size_t index) const {
+template <int N>
+bool bigint<N>::operator[] (std::size_t index) const {
 	return bits[index];
 }
-bigint operator-(bigint object1) {
+template <int N>
+bigint<N> operator-(bigint<N> object1) {
 	//needs optimization
 	object1.bits = ~object1.bits;
 	return object1 + 1;
 }
-bigint operator-(const bigint& object1, const bigint& object2) {
+template <int N>
+bigint<N> operator-(const bigint<N>& object1, const bigint<N>& object2) {
 	return object1 + -object2;
 }
-bigint operator-(const bigint& object1, int primitive1) {
+template <int N>
+bigint<N> operator-(const bigint<N>& object1, int primitive1) {
 	return object1 + -primitive1;
 }
-bigint operator-(int primitive1, const bigint& object1) {
+template <int N>
+bigint<N> operator-(int primitive1, const bigint<N>& object1) {
 	return object1 + -primitive1;
 }
-bool operator<(const bigint& object1, const bigint& object2) {
-	for (int i = 8-1; i >= 0; i--) {
+template <int N>
+bool operator<(const bigint<N>& object1, const bigint<N>& object2) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator<(const bigint& object1, int primitive1) {
+template <int N>
+bool operator<(const bigint<N>& object1, int primitive1) {
 	bigint object2 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator<(int primitive1, const bigint& object2) {
+template <int N>
+bool operator<(int primitive1, const bigint<N>& object2) {
 	bigint object1 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator>(const bigint& object2, const bigint& object1) {
-	for (int i = 8-1; i >= 0; i--) {
+template <int N>
+bool operator>(const bigint<N>& object2, const bigint<N>& object1) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator>(const bigint& object2, int primitive1) {
+template <int N>
+bool operator>(const bigint<N>& object2, int primitive1) {
 	bigint object1 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator>(int primitive1, const bigint& object1) {
+template <int N>
+bool operator>(int primitive1, const bigint<N>& object1) {
 	bigint object2 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return false;
 }
-bool operator<=(const bigint& object1, const bigint& object2) {
-	for (int i = 8-1; i >= 0; i--) {
+template <int N>
+bool operator<=(const bigint<N>& object1, const bigint<N>& object2) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator<=(const bigint& object1, int primitive1) {
+template <int N>
+bool operator<=(const bigint<N>& object1, int primitive1) {
 	bigint object2 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator<=(int primitive1, const bigint& object2) {
+template <int N>
+bool operator<=(int primitive1, const bigint<N>& object2) {
 	bigint object1 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator>=(const bigint& object2, const bigint& object1) {
-	for (int i = 8-1; i >= 0; i--) {
+template <int N>
+bool operator>=(const bigint<N>& object2, const bigint<N>& object1) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator>=(const bigint& object2, int primitive1) {
+template <int N>
+bool operator>=(const bigint<N>& object2, int primitive1) {
 	bigint object1 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator>=(int primitive1, const bigint& object1) {
+template <int N>
+bool operator>=(int primitive1, const bigint<N>& object1) {
 	bigint object2 = primitive1;
-	for (int i = 8-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
         if (object1.bits[i] ^ object2.bits[i]) return object2.bits[i];
     }
     return true;
 }
-bool operator==(const bigint& object1, const bigint& object2) {
+template <int N>
+bool operator==(const bigint<N>& object1, const bigint<N>& object2) {
 	return object1.bits == object2.bits;
 }
-bool operator==(const bigint& object1, int primitive1) {
-	return object1.bits == static_cast<std::bitset<8>>(primitive1);
+template <int N>
+bool operator==(const bigint<N>& object1, int primitive1) {
+	return object1.bits == static_cast<std::bitset<N>>(primitive1);
 }
-bool operator==(int primitive1, const bigint& object1) {
-	return object1.bits == static_cast<std::bitset<8>>(primitive1);
+template <int N>
+bool operator==(int primitive1, const bigint<N>& object1) {
+	return object1.bits == static_cast<std::bitset<N>>(primitive1);
 }
-bigint& bigint::operator+=(bigint object1) {
-	std::bitset<8> temp;
+template <int N>
+bigint<N>& bigint<N>::operator+=(bigint<N> object1) {
+	std::bitset<N> temp;
 	while (!object1.bits.none()) {
 		temp = bits ^ object1.bits;
 		object1.bits = (bits & object1.bits) << 1;
@@ -214,19 +304,23 @@ bigint& bigint::operator+=(bigint object1) {
 	}
 	return *this;
 }
-bigint& bigint::operator+=(int primitive1) {
+template <int N>
+bigint<N>& bigint<N>::operator+=(int primitive1) {
 	operator+=(bigint(primitive1));
 	return *this;
 }
-bigint& bigint::operator-=(const bigint& object1) {
+template <int N>
+bigint<N>& bigint<N>::operator-=(const bigint<N>& object1) {
 	operator+=(-object1);
 	return *this;
 }
-bigint& bigint::operator-=(int primitive1) {
+template <int N>
+bigint<N>& bigint<N>::operator-=(int primitive1) {
 	operator+=(-bigint(primitive1));
 	return *this;
 }
-bigint operator*(const bigint& object1, const bigint& object2) {
+template <int N>
+bigint<N> operator*(const bigint<N>& object1, const bigint<N>& object2) {
 	bigint result;
 	if(object1 < 0 && object2 < 0) {
 		for(int i = 0; i > object2; i--) {
@@ -247,7 +341,8 @@ bigint operator*(const bigint& object1, const bigint& object2) {
 	}
 	return result;
 }
-bigint operator*(const bigint& object1, int primitive1) {
+template <int N>
+bigint<N> operator*(const bigint<N>& object1, int primitive1) {
 	bigint object2 = primitive1;
 	bigint result;
 	if(object1 < 0 && object2 < 0) {
@@ -269,7 +364,8 @@ bigint operator*(const bigint& object1, int primitive1) {
 	}
 	return result;
 }
-bigint operator*(int primitive1, const bigint& object1) {
+template <int N>
+bigint<N> operator*(int primitive1, const bigint<N>& object1) {
 	bigint object2 = primitive1;
 	bigint result;
 	if(object1 < 0 && object2 < 0) {
@@ -291,7 +387,8 @@ bigint operator*(int primitive1, const bigint& object1) {
 	}
 	return result;
 }
-bigint& bigint::operator*=(const bigint& object1) {
+template <int N>
+bigint<N>& bigint<N>::operator*=(const bigint<N>& object1) {
 	bigint object2 = std::move(*this);
 	bits = 0;
 	if(object1 < 0 && object2 < 0) {
@@ -313,7 +410,8 @@ bigint& bigint::operator*=(const bigint& object1) {
 	}
 	return *this;
 }
-bigint& bigint::operator*=(int primitive1) {
+template <int N>
+bigint<N>& bigint<N>::operator*=(int primitive1) {
 	bigint object1 = primitive1;
 	bigint object2 = std::move(*this);
 	bits = 0;
@@ -337,6 +435,7 @@ bigint& bigint::operator*=(int primitive1) {
 	return *this;
 }
 
-bigint operator"" _bi(unsigned long long primitive1) {
-	return bigint(primitive1);
-}
+// TODO figure out how to make this dynamic
+// bigint<N> operator"" _bi(unsigned long long primitive1) {
+// 	return bigint(primitive1);
+// }
